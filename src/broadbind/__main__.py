7d70951fea3b75681858ed887bb969946027e5a6
Avery <ahill15@tulane.edu>
2023-06-25 12:53:53 -0500

    Initial commit

---
+++ src/broadbind/__main__.py
@@ -0,0 +1,118 @@
+import pickle
+import polars as pl
+from pymatgen.core.periodic_table import Element
+from ase.atoms import Atoms
+
+
+def build_vocabulary(systems: list[Atoms]):
+    atom_symbols = []
+    for atoms in systems:
+        symbols = atoms.get_symbols()
+        unique_symbols = set(symbols)
+        atom_symbols.append(unique_symbols)
+    return set(atom_symbols)
+
+
+def process_reaction(key: str, reaction: dict) -> pl.DataFrame:
+    system = reaction["reactionSystems"]
+    atoms = system[key]
+    positions = atoms.get_positions()
+    symbols = atoms.get_chemical_symbols()
+    df = pl.DataFrame(positions, schema=["x", "y", "z"]).with_columns(
+        electron_volts=pl.lit(reaction["reactionEnergy"]), symbol=pl.Series(symbols)
+    )
+    print(df)
+    return df
+
+
+def filter_reactions(reactions: list[dict], products: list[str]) -> dict[dict]:
+    """
+    Filter out any reaction not specified in the products lists
+    """
+    filtered_reactions = {}
+    for reaction in reactions:
+        for key in reaction["reactionSystems"]:
+            if key in products:
+                filtered_reactions[key] = reaction
+    return filtered_reactions
+
+
+def main():
+    products = [
+        "Ostar",
+        "Cstar",
+        "Hstar",
+        "CH3star",
+        "Nstar",
+        "CH2star",
+        "CHstar",
+        "NHstar",
+        "OHstar",
+        "H2Ostar",
+        "SHstar",
+    ]
+
+    pymatgen_element_properties = [
+        # "symbol",
+        # "Z",
+        # "atomic_radius_calculated",
+        # "van_der_waals_radius",
+        # "electrical_resistivity",
+        # "velocity_of_sound",
+        # "reflectivity",
+        # "refractive_index",
+        # "poissons_ratio",
+        # "molar_volume",
+        "thermal_conductivity",
+        # "boiling_point",
+        # "melting_point",
+        # "critical_temperature",
+        # "superconduction_temperature",
+        # "bulk_modulus",
+        # "youngs_modulus",
+        # "brinell_hardness",
+        # "rigidity_modulus",
+        # "mineral_hardness",
+        # "vickers_hardness",
+        # "density_of_solid",
+        # "coefficient_of_linear_thermal_expansion",
+        # "ground_level",
+        # "X"
+        # "atomic_mass",
+        # "atomic_radius",
+        # "average_anionic_radius",
+        "average_cationic_radius",
+        # "average_ionic_radius",
+        # "electron_affinity",
+        "group",
+        # "ionization_energy",
+        # "is_actinoid",
+        # "is_alkali",
+        # "is_alkaline",
+        # "is_chalcogen",
+        # "is_halogen",
+        # "is_lanthanoid",
+        # "is_metal",
+        # "is_metalloid",
+        # "is_noble_gas",
+        # "is_post_transition_metal",
+        # "is_quadrupolar",
+        # "is_rare_earth_metal",
+        # "is_transition_metal",
+        # "max_oxidation_state",
+        # "metallic_radius",
+        "min_oxidation_state",
+        # "row",
+    ]
+
+    with open("data/temp.pickle", "rb") as f:
+        smol_reactions = pickle.load(f)
+
+    filtered_reactions = filter_reactions(reactions=smol_reactions, products=products)
+    for key, reaction in filtered_reactions.items():
+        data = process_reaction(key=key, reaction=reaction)
+        print(data)
+
+
+if __name__ == "__main__":
+    main()
